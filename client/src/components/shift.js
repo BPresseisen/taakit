762c121e220c9e2c045d2a99302a9e3812df9ef9
BPresseisen <benpresseisen@gmail.com>
2019-07-11 23:13:54 -0400

    latestGreatest

---
+++ client/src/components/shift.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class Shift extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col s12">
+                        Welcome to Taakit, a solution for the future of in home assisted care. <br/>
+                        <br/>
+                        Why we created Taakit?<br/>
+                        <br/>
+                        <ul className="collection ">
+                            <li className="collection-item">There are approximately 80MM baby boomers (born between 1946 and 1965)
+</li>
+<li className="collection-item ">
+As these boomers age, many will require aided care to live their lives as independently as possible and to live with the dignity of remaining in their own home.
+
+</li>
+<li className="collection-item">
+Taking care of loved ones who need aided care is difficult in and of itself. With TAAKIT those who are tasked with such responsibilities can benefit from a platform that maps and manages everything which needs doing and monitoring.
+
+</li>
+<li className="collection-item">
+With TAAKIT, loved ones receive the care they need and in real-time these tasks are planned, charted, and communicated as they are delivered.
+
+</li>
+<li className="collection-item">
+real time data in the cloud empowers, enables and educates a multitude of people who may fulfill the caregiving role whether they are an at-home aide, nurse practitioner or from among concerned family and friends 
+
+</li>
+<li className="collection-item">
+The TAAKIT platform is mobile friendly and reduces waste, miscommunications, burdensome duplicate and triplicate paperwork and the manual errors that underlie existing systems, inefficiencies and procedures used by caregivers
+
+</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Shift
--- models/models/shift.js
+++ models/models/shift.js
@@ -2,52 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ShiftSchema = new Schema({
-  timeOut: { type: Date, default: Date.now },
+  Date: {type: Date},
+  Shift: {type: String, required: true},
   userID:  { type: Number, required: true },
-  
-  //MEMORY VITALS QUESTION
-  //-->PRIOR DAY SCHEDULE
-  //pMemory: {type: Number, required: false},
-  //-->CURRENT DAY SCHEDULE
-  //-->NEXT DAY SCHEDULE
-  //-->OTHER MEMORY QUESTIONS
-
-  //JOINT PAIN VITALS QUESTION
-  // jPain: {type: Number, required: false},
-  // //MUSCULAR PAIN VITALS QUESTION
-  // mPain: {type: Number, required: false},
-  // //OTHER PAIN (SWELLING) VITALS QUESTION
-  // oPain: {type: Number, required: false},
-  // //APPETITE VITALS QUESTION
-  // appetite: {type: Number, required: false},
-  // //ENERGY VITALS QUESTION
-  // energy: {type: Number, required: false},
-  // //SLEEP QUALITY VITALS QUESTION
-  // sleepQual: {type: Number, required: false},
-  // //FATIGUE-SLEEPINESS VITALS QUESTION
-  // fatigue: {type: Number, required: false},
-  // //MOBILITY VITALS QUESTION
-  // mobility: {type: Number, required: false},
-  // //BALANCE VITALS QUESTION
-  // balance: {type: Number, required: false},
-  // //BREATHING QUALITY
-  // breathQual:{type: Number, required: false},
-  // //CALMNESS-MOOD
-  // calmMood:{type: Number, required: false},
-  // //TECH MODALITY VITALS QUESTION
-  // tModality: {type: Number, required: false},
-  // //CONTINENCE VITALS QUESTION--Regular or Constipated
-  // bowels: {type: Number, required: false},
-  // //GASTROINTESTINAL VITALS QUESTION
-  // gastro: {type: Number, required: false},
-  // //VITALS COMPOSITE SCORE
-  // vitalsScore: {type: Number, required: false},
-  // //MEMO
-  // memo: { type: String, required: false }
-
-  
+  patientName: {type: String, required: true},
+  role: {type: Number, required: true},
+  aideCaregiverID: {type:Number, required: true},
+  aideCaregiverName: {type: String, required: true},
+  task: {type: Array, required: true},
+  taskScore: {type:Array, required:true},
+  VitalScore: { type: Number, required: true },
+  /// VITALS DETAIL???
+  note: { type: String, required: false }
 });
 
-const Task = mongoose.model("Task", ShiftSchema);
+const Task = mongoose.model("Shift", ShiftSchema);
 
-module.exports = Task;
+module.exports = Shift;
